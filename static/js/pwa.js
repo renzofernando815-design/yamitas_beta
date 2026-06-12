53d0fa409a789254c9a534e33a11bf7dc2b47609
refexgg <renzoc4rrillo88@gmail.com>
2026-06-11 22:06:24 -0300

    Preparar proyecto para GitHub y PWA

---
+++ static/js/pwa.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/static/sw.js')
+      .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
