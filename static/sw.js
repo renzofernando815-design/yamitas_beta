8043d64ed17c249cd34eed947ab99120d4ad35e5
refexgg <renzoc4rrillo88@gmail.com>
2026-06-26 00:57:57 -0300

    prueba quince(no preguntar)

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'yamitas-cache-v1';
+const CACHE_NAME = 'yamitas-cache-v2';
 const URLsToCache = [
   '/',
   '/static/css/style.css',
+  '/static/css/bootstrap.min.css',
   '/static/manifest.json'
 ];
 
@@ -9,6 +10,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(URLsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
